a2bcbe0dec97bfca8d72291ce870e27db2d52d8b
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2026-02-17 23:51:14 +0530

    application api

--- src/routes/applicationRoute.js
+++ src/routes/applicationRoute.js
@@ -1,23 +1,17 @@
 import express from "express";
-import { createApplication, getApplications } from "../controllers/applicationController.js";
-import authMiddleware from "../middleware/auth.js";
-import authorizeRoles from "../middleware/role.js";
+import { createApplication, getApplications } from "../controllers/applicationControllers.js";
 
 const applicationRoute = express.Router();
 
 // Students can submit applications
 applicationRoute.post(
-  "/",
-  authMiddleware,
-  authorizeRoles("student"),
+  "/user-apply",
   createApplication
 );
 
 // Admin can view all applications
 applicationRoute.get(
   "/",
-  authMiddleware,
-  authorizeRoles("admin"),
   getApplications
 );
 
